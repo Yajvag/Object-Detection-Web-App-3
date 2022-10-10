ef4d8ea3ad7c01280a240daef771257f837ee8fe
YAJ <96839995+Yajvag@users.noreply.github.com>
2022-10-10 16:53:42 -0700

    Add files via upload

---
+++ iphone.js
@@ -0,0 +1,57 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage("iphone.webp");
+}
+
+function setup() {
+    canvas = createCanvas(640, 490);
+    canvas.center();
+ objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function draw() {
+    image(img, 0, 0, 640, 490);
+
+    if(status != "") {
+        for(i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        document.getElementById("status2").innerHTML = "There is 1 big object in the image from which cocossd model has detected 1 object";
+    }
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function changeColor() {
+    document.getElementById("back_button").style.color = "red";
+}
+
+function resetColor() {
+    document.getElementById("back_button").style.color = "white";
+}
+
+function back() {
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function MR() {
+      window.location = "monitor.html";
+}
+
+function IP() {
+    window.location = "iphone.html";
+}
+
+function scissor() {
+    window.location = "scissors.html";
+}
+
+function Plane() {
+    window.location = "plane.html";
+}
+
+function bottle() {
+    window.location = "bottle.html";
+}
